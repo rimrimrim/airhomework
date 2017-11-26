a5f4fbfdf192658757d8f66d1d1de04aeef26db5
takanori midorikawa <takanorimidorikawa@takanori-no-MacBook-Air.local>
2017-11-26 22:23:15 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  def index
+    render html: "hello, world!"
+  end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phone_number, :description])
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+
+  get 'title/ok'
+
+  root 'titles#index'
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+   end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
+
+  resources :titles
+
+  root to: "home#index"
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
